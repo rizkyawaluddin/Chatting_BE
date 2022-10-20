1c6d2e5067747f79619ef3a617d53e995366aff0
rizkyawaluddin <rizkyawaluddin010498@gmail.com>
2022-10-20 19:32:08 +0700

    update

--- index.js
+++ index.js
@@ -19,12 +19,25 @@ const io = new Server(server, {
 
 require('./src/socket')(io);
 
-const PORT = process.env.PORT || 5000
+require('dotenv').config()
+
+const { Pool } = require('pg')
+const isProduction = process.env.NODE_ENV === 'production'
+
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+
+const pool = new Pool({
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: isProduction,
+})
+
+module.exports = { pool }
+// const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(cors())
 
-app.use('/api/v1/', router)
+app.use('/api/', router)
 app.get('/', function (req, res) {
   res.send({
       message: 'Hello World'
